72aaad244714bf33556b5ff062b55dce44f7fe48
Casainho <casainho@gmail.com>
2012-10-27 19:35:08 +0100

    Small change.

--- firmware/bldc.h
+++ firmware/bldc.h
@@ -9,11 +9,5 @@
  * Released under the GPL License, Version 3
  */
 
-void commutation_sector_1 (void);
-void commutation_sector_2 (void);
-void commutation_sector_3 (void);
-void commutation_sector_4 (void);
-void commutation_sector_5 (void);
-void commutation_sector_6 (void);
 void commutation_disable (void);
 void commutation (void);
