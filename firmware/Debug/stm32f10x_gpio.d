3bf99090a5708a3c3a488369181f867b45738028
Casainho <casainho@gmail.com>
2013-01-15 22:59:34 +0000

    This first code for STM32F100 builds but is UNTESTED.

---
+++ firmware/Debug/stm32f10x_gpio.d
@@ -0,0 +1,15 @@
+stm32f10x_gpio.d: ../stm32f10x_gpio.c ../stm32f10x_gpio.h \
+ ../stm32f10x_map.h ../stm32f10x_conf.h ../stm32f10x_type.h \
+ ../cortexm3_macro.h ../stm32f10x_rcc.h
+
+../stm32f10x_gpio.h:
+
+../stm32f10x_map.h:
+
+../stm32f10x_conf.h:
+
+../stm32f10x_type.h:
+
+../cortexm3_macro.h:
+
+../stm32f10x_rcc.h:
